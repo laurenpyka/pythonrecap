81674f2f8bf356acc93bb2b3648fc7d0f71c5122
Lauren <lauren_pyka1@baylor.edu>
2024-01-24 13:29:02 -0600

    updated gitignore and changed print statement

--- MyGraph.py
+++ MyGraph.py
@@ -6,4 +6,10 @@ plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
+print("Hello World!")
+
+# create virtual environment for interpreter and install modules there 
+#1. create a virtual environment -py for PCs, for macs python3. (EX: python3 -m venv myvenv)
+# make sure youre in the virtual root environment
+#2. activate your virtual environment -> source myvenv/bin/activate
+#3. install 3rd party library or module -> pip3 install matplotlib
